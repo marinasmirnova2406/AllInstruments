e279664b0c62908b7cd6e024ade374b0780ee9f2
Marina Smirnova <marinasmirnova2406@gmail.com>
2020-03-29 12:41:53 +0300

    Закончена логика создания и наполнения ячеек

--- scripts.js
+++ scripts.js
@@ -2,15 +2,17 @@
 let tests =  {
     virtual:[
       ["Mary","Office","Manager","2019-11-04T07:00:00.000Z","2019-11-04T18:0000.000Z"],
-      ["Kate","Office","Manager","2019-11-04T09:00:00.000Z","2019-11-04T20:2500.000Z"],
-      ["Anne","Reception","Admin","2019-11-04T12:00:00.000Z","2019-11-04T20:0000.000Z"],
-      ["nameN","placeN","role1","datetime_fromN","datetime_toN"]
+      ["Kate","Office","Manager","2019-11-04T09:00:00.000Z","2019-11-04T20:1500.000Z"],
+      ["Anne","Reception","Admin","2019-11-04T08:20:00.000Z","2019-11-04T20:3000.000Z"],
+      ["July","Office","HR","2019-11-04T08:20:00.000Z","2019-11-04T08:50000.000Z"],
+      //["nameN","placeN","role1","datetime_fromN","datetime_toN"]
     ],
     actual:[
       ["Mary","Office","Manager","2019-11-04T07:00:00.000Z","2019-11-04T18:0000.000Z"],
       ["Kate","Office","Manager","2019-11-04T09:00:00.000Z","2019-11-04T22:0000.000Z"],
       ["Anne","Reception","Admin","2019-11-04T13:00:00.000Z","2019-11-04T19:0000.000Z"],
-      ["nameN","placeN","role1","datetime_fromN","datetime_toN"]
+      ["July","Office","HR","2019-11-04T08:20:00.000Z","2019-11-04T20:3000.000Z"],
+      //["nameN","placeN","role1","datetime_fromN","datetime_toN"]
     ]
   }
 
@@ -24,25 +26,30 @@ let tests =  {
     };
 
 
-    var timeTable = document.getElementById("timetable"); // Инициализируем таблицу
-    var newRow = document.createElement("tr"); // Создаем новую строку
+    let timeTable = document.getElementById("timetable"); // Инициализируем таблицу
+    let newRow = document.createElement("tr"); // Создаем новую строку
 
     // Создаем первую ячейку со значением "Имя"
-    var readName = arrVariant[string][0];
-    var nameCell=document.createElement("td");
+    let readName = arrVariant[string][0];
+    let nameCell=document.createElement("td");
     nameCell.innerHTML = readName;
     newRow.appendChild(nameCell);
 
     // Создаем первую ячейку со значением "Расположение и роль"
-    var readPlase = arrVariant[string][1];
-    var readRole = arrVariant[string][2];
-    var placeRoleCell=document.createElement("td");
+    let readPlase = arrVariant[string][1];
+    let readRole = arrVariant[string][2];
+    let placeRoleCell=document.createElement("td");
     placeRoleCell.innerHTML = readPlase + " / " + readRole;
     newRow.appendChild(placeRoleCell);
 
     timeTable.appendChild(newRow);
 
 
+    // -----------------------------
+    // -----------------------------
+    // считывание времени ----------
+    // -----------------------------
+    // -----------------------------
     function timeHandler(variant, string) {
       let readStringF = variant[string][3];
       let readStringT = variant[string][4];
@@ -61,120 +68,143 @@ let tests =  {
         minute: readStringT[14] + readStringT[15]
       };
 
+      let differenceHour = timeTo.hour - timeFrom.hour;
+
+
+      // -----------------------------
+      // -----------------------------
+      // функции создания ячеек ------
+      // -----------------------------
+      // -----------------------------
+
       // Создание пустой ячейки
       function empty() {
-        var emptyCell=document.createElement("td");
+        let emptyCell=document.createElement("td");
         emptyCell.innerHTML = "e";
         newRow.appendChild(emptyCell);
       };
 
-      if (timeFrom.minute == "00") {
-        let differenceHour = timeTo.hour - timeFrom.hour;
+      // Создание ячейки "Рабочее время"
+      function work() {
+        let readName = "abc";
+        let colorCell=document.createElement("td");
+        colorCell.innerHTML = readName;
+        colorCell.setAttribute('class', colorClass);
+        newRow.appendChild(colorCell);
+      };
 
-        // Добавляем ячейки "До рабочео времени", не отнимает 1, т.к. отсчет начинается с 00, а не с 01.
-        for (var i = 0; i < timeFrom.hour ; i++) {
-          empty();
-        };
+      // Определение того, нужно ли создавать не полный рабочий час ВНАЧАЛЕ рабочего создания
+      // И его создание
+      function partHourBefore() {
+        let partCell=document.createElement("td");
+        let reverceMinute = 60 - timeFrom.minute;
+        let percent = Math.round(reverceMinute * 100 / 60);
+        partCell.setAttribute('style', 'text-align: right;');
+        partCell.innerHTML = '<div class="' + colorClass + '"style="display: inline-block; height: 100%; width: ' + percent + '%;"></div>';
+        newRow.appendChild(partCell);
+      }
 
-        // Добавляем ячейки "Рабочее время"
-        for (var i = 0; i < differenceHour; i++) {
-          var readName = "abc";
-          var colorCell=document.createElement("td");
-          colorCell.innerHTML = readName;
-          colorCell.setAttribute('class', colorClass);
-          newRow.appendChild(colorCell);
-        };
-
+      // Определение того, нужно ли создавать не полный рабочий час ВКОНЦЕ рабочего создания
+      // И его создание
+      function partHourAfter() {
         // если минуты конечного времени 00
         if (timeTo.minute == "00") {
           for (var i = 0; i < 24 - timeTo.hour; i++) {
             empty();
           };
 
-
           // если минуты конечного времени не 00
         } else {
-          var partCell=document.createElement("td");
+
+          // добавление ячейки с неполным часом "после"
+          let partCell=document.createElement("td");
           let percent = Math.round(timeTo.minute * 100 / 60);
           partCell.innerHTML = '<div class="' + colorClass + '" style="height: 100%; width: ' + percent + '%;"></div>';
           newRow.appendChild(partCell);
 
+          // добавление пустых яеек после рабочего времени
           for (var i = 0; i < 23 - timeTo.hour; i++) {
             empty();
           };
         };
-
-
-      } else { // если начальное время не 00
-
-
-
-
-
-
-
-
-        return "NO!";
       };
 
+      if (differenceHour == 0) {
 
+        // Добавляем ячейки "До рабочео времени"
+        for (var i = 0; i < timeFrom.hour ; i++) {
+          empty();
+        };
+
+        let partCell=document.createElement("td");
+        let minuteLessHour = timeTo.minute - timeFrom.minute;
+        let percent = Math.round(minuteLessHour * 100 / 60);
+        let prePercent = Math.round(timeFrom.minute * 100 / 60);
+        partCell.innerHTML = '<div style="display: inline-block; height: 100%; width: ' + prePercent + '%;"></div><div class="' + colorClass + '"style="display: inline-block; height: 100%; width: ' + percent + '%;"></div>';
+        newRow.appendChild(partCell);
+
+        // Добавляем ячейки "После рабочео времени"
+        for (var i = 0; i < 23 - timeTo.hour; i++) {
+          empty();
+        };
+
+      // Если рабочее время больше часа
+      } else {
+        // -----------------------------
+        // -----------------------------
+        // если начальное время 00 минут
+        // -----------------------------
+        // -----------------------------
+        if (timeFrom.minute == "00") {
+
+          // Добавляем ячейки "До рабочео времени"
+          for (var i = 0; i < timeFrom.hour ; i++) {
+            empty();
+          };
+
+          // Добавляем ячейки "Рабочее время"
+          for (var i = 0; i < differenceHour; i++) {
+            work();
+          };
+
+          // Вызов функции пределения неполного часа + создание ячеек "после"
+          partHourAfter();
+
+        // --------------------------
+        // --------------------------
+        // если начальное время не 00
+        // --------------------------
+        // --------------------------
+        } else {
+
+          // Добавляем ячейки "До рабочео времени", не отнимаем 1, т.к. отсчет начинается с 00, а не с 01.
+          for (var i = 0; i < timeFrom.hour ; i++) {
+            empty();
+          };
+
+          // добавление ячейки с неполным часом
+          partHourBefore();
+
+          // Добавляем ячейки "Рабочее время"
+          for (var i = 0; i < differenceHour - 1; i++) {
+            work();
+          };
+
+          // Вызов функции пределения неполного часа + создание ячеек "после"
+          partHourAfter();
+
+        };
+
+      }; // else (рабочее время больше часа)
 
     }; // fun timeHandler
 
-
-
-timeHandler(arrVariant, string);
-//изменить HTML содержимое элемента, имеющего id="myP"
-//myP.innerHTML = differenceHour;
-
+  timeHandler(arrVariant, string);
 
 }; // fun treRow
 
-   theRow("virtual", 0);
-   theRow("virtual", 1);
-   theRow("virtual", 2);
-   theRow("virtual", 3);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* function addRow(name) {
-     var timeTable = document.getElementById("timetable"); // Инициализируем таблицу
-     var newRow = document.createElement("tr"); // Создаем новую строку
-
-
- for (var i=0; i<26; i++) {
-  var newCell=document.createElement("td");
-  newCell.setAttribute('class', ipCreator + i);
-  newCell.innerHTML = name;
-  ipCreator ++;
-
-
-  newRow.appendChild(newCell);
- }
- timeTable.appendChild(newRow);
-} */
+for (var i = 0; i < tests.virtual.length; i++) {
+  theRow("virtual", i);
+}
